e25b8c9a2d8d363016f144839f5c1a4dcfebcb6e
Burak Üren <burakuren101@gmail.com>
2022-10-07 13:07:46 +0300

    loop is working fine, all the restaurants are signed in, waiting for the table design so that I write the data to the table accoring to that

--- app.py
+++ app.py
@@ -5,6 +5,108 @@ import requests
 from datetime import datetime
 from openpyxl import load_workbook
 
+def getir_to_excel_first():
+
+    myFileName=r'./Sheet.xlsx'
+
+    wb = load_workbook(filename=myFileName)
+    
+    #TODO: gonna create the page for every restaurant and make the changes into those restaurants
+    ws = wb['Sheet1']
+    
+    #TODO: Be sure that all the restaurant are open! And get the links. Then create a if statement to make OPEN/CLOSE diff. Belove statement is not quite right, gotta change it with the real time case.
+    url_dict = {
+        "anan":"https://lkdsakjfda.com",
+        "Alle Bowls":"https://getir.com/yemek/restoran/alle-bowls-acibadem-mah-kadikoy-istanbul/",
+        "Arianas Cheesecake":"https://getir.com/yemek/restoran/ariana-s-cheesecake-acibadem-mah-kadikoy-istanbul/",
+        "Big Bold Quick":"https://getir.com/yemek/restoran/bbq-big-bold-quick-acibadem-mah-kadikoy-istanbul",
+        "Caesar Salad By":"https://getir.com/yemek/restoran/caesar-salad-by-chef-amadeo-acibadem-mah-kadikoy-istanbul",
+        "Çosa":"https://getir.com/yemek/restoran/cosa-bi-corba-bi-salata-acibadem-mah-kadikoy-istanbul/",
+        "Detroit Bad Boys Pizza":"https://getir.com/yemek/restoran/detroit-bad-boys-pizza-acibadem-mah-kadikoy-istanbul",
+        "Dlycious Dyssert": "https://getir.com/yemek/restoran/dydy-dylicious-dyssert-acibadem-mah-kadikoy-istanbul",
+        "Doyuyo":"https://getir.com/yemek/restoran/doyuyo-sarayardi-cad-kadikoy-istanbul/",
+        "El Pollo Lasso": "https://getir.com/yemek/restoran/el-pollo-lasso-acibadem-mah-kadikoy-istanbul/",
+        "Etişler Köfte" : "https://getir.com/yemek/restoran/et-isleri-kofte-burger-durum-acibadem-mah-kadikoy-istanbul/",
+        "Fadelini" : "https://getir.com/yemek/restoran/fadelini-acibadem-mah-kadikoy-istanbul/",
+        "Fun For Fit": "https://getir.com/yemek/restoran/fun-for-fit-acibadem-mah-kadikoy-istanbul",
+        "G&G Burger": "https://getir.com/yemek/restoran/g-g-burger-acibadem-mah-kadikoy-istanbul-2/",
+        "Gurra Tavuk": "https://getir.com/yemek/restoran/gurra-tavuk-doner-acibadem-mah-kadikoy-istanbul",
+        "Jay Jay Fries": "https://getir.com/yemek/restoran/jay-jay-fries-acibadem-mah-kadikoy-istanbul",
+        "Kale Arkası Mutfak": "https://getir.com/yemek/restoran/kale-arkasi-mutfak-acibadem-mah-kadikoy-istanbul",
+        "Kengeres Çiğ Köfte" : "https://getir.com/yemek/restoran/kengeres-gurme-cig-kofte-acibadem-mah-kadikoy-istanbul",
+        "Madritas": "https://getir.com/yemek/restoran/madritas-acibadem-mah-kadikoy-istanbul",
+        "Mztps Meze" : "https://getir.com/yemek/restoran/mztps-meze-tapas-acibadem-mah-kadikoy-istanbul",
+        "Nane Mantı" : "https://getir.com/yemek/restoran/nane-manti-acibadem-mah-kadikoy-istanbul/",
+        "Noody" : "https://getir.com/yemek/restoran/noody-acibadem-mah-kadikoy-istanbul/",
+        "Red Haag" : "https://getir.com/yemek/restoran/red-haag-cafe-brasserie-acibadem-mah-kadikoy-istanbul",
+        "Rylee's Ranch Salad": "https://getir.com/yemek/restoran/rylee-s-ranch-salad-acibadem-mah-kadikoy-istanbul/",
+        "Seez Beez": "https://getir.com/yemek/restoran/seez-beez-falafel-wraps-burgers-acibadem-mah-kadikoy-istanbul",
+        "Senor Torreon":"https://getir.com/yemek/restoran/senor-torreon-red-hot-spicy-food-acibadem-mah-kadikoy-istanbul/",
+        "Tabur Köfte":"https://getir.com/yemek/restoran/tabur-kofte-acibadem-mah-kadikoy-istanbul",
+        "The Bowl":"https://getir.com/yemek/restoran/the-bowl-best-of-we-love-acibadem-mah-kadikoy-istanbul",
+        "Veganista" : "https://getir.com/yemek/restoran/veganista-acibadem-mah-kadikoy-istanbul"
+
+    }
+
+    for url_key in url_dict.keys():
+        url = url_dict[url_key]
+        url_key = url_key
+        r = None
+
+        #This is for 'if connection get suspened by getir.com because we are making so much request in a short time'
+        try:
+            r = requests.get(url)
+        except Exception as e:
+            print(e)
+            continue
+        
+        if r == None:
+            print("URL is None")
+            continue
+
+        #TODO: This is gonna change, because we are not checking the restauant open/close status by status.code normally. # Have the screenshot in the screenshots.
+
+        soup = BeautifulSoup(r.content, 'html.parser')
+
+        close = get_close(soup=soup)
+
+        date = datetime.now()
+
+
+        try:
+            current_rating = get_rating(soup=soup)
+
+        except Exception as e:
+
+            current_rating = "NO RATING"
+            print(e)
+        
+        # Check that if close state is exsist in the html.
+        if close == None:            
+            #TODO: append the date to the current sheet. But this is gonna change by the restaurant.
+            ws.append([url_key, date, "AÇIK" , current_rating])
+
+            wb.save(filename=myFileName)
+
+            wb.close()
+
+            print("Changes saved!")
+        
+        #TODO: This is gonna be canceled because we are not going to calculate the current OPEN/CLOSE status with this
+        else:
+
+            ws.append([url_key ,date, "KAPALI" , current_rating])
+
+            wb.save(filename=myFileName)
+    
+            wb.close()
+
+            print("Changes saved!")        
+        sleep(5)
+
+    return url_dict # temp
+
+
 def get_rating(soup):
         #Find the rate
         s = soup.find("div", id = "__next")
@@ -41,12 +143,7 @@ def get_close(soup):
 
     return close
 
-def get_name(url):
-
-    return url.split("/")[-2]
-
-
-@repeat(every(2).second) #.until("18:30")
+@repeat(every(1).hour) #.until("18:30")
 def getir_to_excel():
 
     myFileName=r'./Sheet.xlsx'
@@ -90,18 +187,17 @@ def getir_to_excel():
     }
 
     for url_key in url_dict.keys():
-
-        '''
+        url = url_dict[url_key]
+        
         #This is for 'if connection get suspened by getir.com because we are making so much request in a short time'
         try:
-            r = requests.get(i)
-        except requests.exceptions.ConnectionError:
-            sec = 60*3
-            sleep(sec)
-        '''
-        url = url_dict[url_key]
+            r = requests.get(url)
+        except requests.exceptions.ConnectionError as er:
+            print(er)
+        
 
-        r = requests.get(url)
+
+        #r = requests.get(url)
 
         #normal status code checks
         if r.status_code == 404:
@@ -118,7 +214,7 @@ def getir_to_excel():
 
         date = datetime.now()
 
-        name = get_name(url=url)
+        url_key = get_name(url=url)
 
         try:
             current_rating = get_rating(soup=soup)
@@ -131,7 +227,7 @@ def getir_to_excel():
         # Check that if close state is exsist in the html.
         if close == None:            
             #TODO: append the date to the current sheet. But this is gonna change by the restaurant.
-            ws.append([name, date, "AÇIK" , current_rating])
+            ws.append([url_key, date, "AÇIK" , current_rating])
 
             wb.save(filename=myFileName)
 
@@ -142,7 +238,7 @@ def getir_to_excel():
         #TODO: This is gonna be canceled because we are not going to calculate the current OPEN/CLOSE status with this
         else:
 
-            ws.append([name ,date, "KAPALI" , current_rating])
+            ws.append([url_key ,date, "KAPALI" , current_rating])
 
             wb.save(filename=myFileName)
     
@@ -154,6 +250,11 @@ def getir_to_excel():
 
     return url_dict # temp
 
+
+
+
+#for the first time and then let the schdule does it's job
+getir_to_excel_first()
 
 while True:
     run_pending()
@@ -173,7 +274,7 @@ def get_name(soup):
     s3 = s4.find("div", class_ = "style__CardWrapper-sc-__sc-sbxwka-12 iBBNFu")
     s2 = s3.find("div", class_ = "style__ContentWrapper-sc-__sc-sbxwka-7 fIhlvO")
     s1 = s2.find("div", class_ = "sc-7047f3e2-0 ggYwVs")
-    name = s1.find("h1", class_="style__Title1-sc-__sc-1nwjacj-2 hIkhWh sc-7047f3e2-2 hAeUHj")
+    url_key = s1.find("h1", class_="style__Title1-sc-__sc-1nwjacj-2 hIkhWh sc-7047f3e2-2 hAeUHj")
 
-    return name
+    return url_key
 '''
